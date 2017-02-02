56dbd7b3859e966d596900911fb25831a57e20db
Ruy Garcia <rrgarciach@gmail.com>
2017-02-02 02:04:40 -0700

    Removed body parser.

--- modules/api/index.js
+++ modules/api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { ParseServer } from 'parse-server';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 
 import parseDashboard from '../dashboard';
 
