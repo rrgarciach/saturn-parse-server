871113b4e4f0c0d951a1cd220aecf7b01d70322b
Ruy Garcia <rrgarciach@gmail.com>
2017-03-09 21:37:19 -0700

    SAT-19: Added cloud method for Orders to add ACL

--- cloud/order.cloud.js
+++ cloud/order.cloud.js
@@ -1,6 +1,7 @@
 const q = require('q');
 
 import Order from '../models/order.model';
+import Client from '../models/client.model';
 
 module.exports.load = () => {
 
@@ -9,10 +10,57 @@ module.exports.load = () => {
         let order = req.object;
 
         if (req.user) {
-            calcOrderTotals(order)
+            let sessionToken = req.user.get('sessionToken');
+
+            // let roleQuery = new Parse.Query('_Role');
+            // roleQuery
+            //     .equalTo('name', 'Operations')
+            //     .first()
+            //     .then(_operationsRole => {
+                    // console.log('_operationsRole', _operationsRole.getUsers())
+
+                    // operationsRole = _operationsRole;
+                    let client = order.client;
+                    let clientQuery = new Parse.Query(Client);
+                    return clientQuery
+                        .equalTo('objectId', client.id)
+                        .include('promoter')
+                        .include('user')
+                        .first()
+                // })
+                .then(client => {
+                    // try {
+                        console.log('client', client);
+                        console.log('client.promoter', client.promoter);
+                        console.log('client.user', client.user);
+
+                        if (!order.existed()) {
+                            let orderACL = new Parse.ACL();
+                            orderACL.setWriteAccess(client.user, true);
+                            orderACL.setReadAccess(client.user, true);
+                            orderACL.setWriteAccess(client.promoter.id, true);
+                            orderACL.setReadAccess(client.promoter.id, true);
+                            orderACL.setRoleReadAccess('Operations', true);
+                            orderACL.setRoleWriteAccess('Operations', true);
+                            // console.log('orderACL', orderACL)
+                            order.setACL(orderACL);
+                        }
+                        console.log('order.existed()', order.existed())
+                        console.log('order.getACL()', order.getACL())
+                    // } catch (e) {
+                    //     console.error(e)
+                    // }
+
+                    // check if request came from a User
+
+                    return calcOrderTotals(order);
+                    // return Promise.resolve(order);
+
+                })
                 .then(order => {
 
                     if (!order.get('folio')) {
+                    console.log('another then')
 
                         let query = new Parse.Query(Order);
                         query.limit(1);
@@ -22,7 +70,8 @@ module.exports.load = () => {
                                 console.log('order', _order, _order.get('folio'), _order.folio)
                                 let maxValue = _order.get('folio');
                                 order.set('folio', ++maxValue);
-                                order.save()
+                                // order.save(null, {useMasterKey: true})
+                                order.save(null, {sessionToken: sessionToken})
                                     .then(() => {
                                         res.success();
                                     });
@@ -31,7 +80,7 @@ module.exports.load = () => {
                         // res.error('you cannot give less than one star');
 
                     } else {
-                        order.save()
+                        order.save(null, {useMasterKey: true})
                             .then(() => {
                                 res.success();
                             });
@@ -39,8 +88,8 @@ module.exports.load = () => {
                     }
 
                 });
-        }
 
+        }
 
     });
 
