bee3adca9ac93594a8235a35e3e45beaa0c3d658
Ruy Garcia <rrgarciach@gmail.com>
2017-03-09 21:37:37 -0700

    SAT-19: Fixed Roles migration

--- migrations/20170219161900-create-roles.js
+++ migrations/20170219161900-create-roles.js
@@ -5,15 +5,54 @@ module.exports.up = () => {
     let roleACL = new Parse.ACL();
     roleACL.setPublicReadAccess(true);
 
+    let guestRole = new Parse.Role('Guest', roleACL);
+
+    let clientRole = new Parse.Role('Client', roleACL);
+
+    let promoterRole = new Parse.Role('Promoter', roleACL);
+
+    let operationsRole = new Parse.Role('Operations', roleACL);
+
+    let managerRole = new Parse.Role('Manager', roleACL);
+
+    let adminRole = new Parse.Role('Administrator', roleACL);
+
     let roles = [];
-    roles.push( new Parse.Role('Administrator', roleACL) );
-    roles.push( new Parse.Role('Promoter', roleACL) );
-    roles.push( new Parse.Role('Client', roleACL) );
-    roles.push( new Parse.Role('Guest', roleACL) );
+    roles.push(guestRole);
+    roles.push(clientRole);
+    roles.push(promoterRole);
+    roles.push(operationsRole);
+    roles.push(managerRole);
+    roles.push(adminRole);
 
     return Parse.Object.saveAll(roles, {
-        success: () => {
-            console.info('Roles successfully created.');
+        success: results => {
+            try {
+
+                let operationsRole = results[3];
+                let managerRole = results[4];
+
+                operationsRole.getRoles().add([managerRole]);
+                managerRole.getRoles().add([adminRole]);
+
+                let rolesRelations = [];
+                rolesRelations.push(operationsRole);
+                rolesRelations.push(managerRole);
+
+                return Parse.Object.saveAll(rolesRelations, {
+                    useMasterKey: true,
+                    success: () => {
+                        console.info('Roles successfully created.');
+                    },
+                    error: error => {
+                        console.error(error.message, error);
+                    },
+                });
+
+
+            } catch (e) {
+                console.error(e);
+            }
         },
         error: error => {
             console.error(error.message, error);
@@ -25,21 +64,5 @@ module.exports.up = () => {
 module.exports.down = () => {
     console.info('Rolling back Roles...');
 
-    let roleACL = new Parse.ACL();
-    roleACL.setPublicReadAccess(false);
-
-    let roles = [];
-    roles.push( new Parse.Role('Administrator', roleACL) );
-    roles.push( new Parse.Role('Promoter', roleACL) );
-    roles.push( new Parse.Role('Client', roleACL) );
-    roles.push( new Parse.Role('Guest', roleACL) );
-
-    return Parse.Object.saveAll(roles, {
-        success: () => {
-            console.info('Roles successfully removed.');
-        },
-        error: error => {
-            console.error(error.message, error);
-        },
-    });
+    // @TODO: Add rollback for this Roles migration
 };
