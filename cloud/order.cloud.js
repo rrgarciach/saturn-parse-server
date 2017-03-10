e94f3b4971f622e0914317f65c37788a591c5859
Ruy Garcia <rrgarciach@gmail.com>
2017-03-09 21:56:25 -0700

    SAT-19: Refactored Order’s cloud.

--- cloud/order.cloud.js
+++ cloud/order.cloud.js
@@ -1,5 +1,3 @@
-const q = require('q');
-
 import Order from '../models/order.model';
 import Client from '../models/client.model';
 
@@ -9,75 +7,39 @@ module.exports.load = () => {
 
         let order = req.object;
 
+        // Check if this save came from a User in order to avoid and infinite loop after saving here:
         if (req.user) {
-            let sessionToken = req.user.get('sessionToken');
+            let sessionToken = req.user.get('sessionToken'); // get user session token
 
-            // let roleQuery = new Parse.Query('_Role');
-            // roleQuery
-            //     .equalTo('name', 'Operations')
-            //     .first()
-            //     .then(_operationsRole => {
-                    // console.log('_operationsRole', _operationsRole.getUsers())
+            // Set Order's ACL:
+            return setOrderACL(order)
 
-                    // operationsRole = _operationsRole;
-                    let client = order.client;
-                    let clientQuery = new Parse.Query(Client);
-                    return clientQuery
-                        .equalTo('objectId', client.id)
-                        .include('promoter')
-                        .include('user')
-                        .first()
-                // })
-                .then(client => {
-                    // try {
-                        console.log('client', client);
-                        console.log('client.promoter', client.promoter);
-                        console.log('client.user', client.user);
-
-                        if (!order.existed()) {
-                            let orderACL = new Parse.ACL();
-                            orderACL.setWriteAccess(client.user, true);
-                            orderACL.setReadAccess(client.user, true);
-                            orderACL.setWriteAccess(client.promoter.id, true);
-                            orderACL.setReadAccess(client.promoter.id, true);
-                            orderACL.setRoleReadAccess('Operations', true);
-                            orderACL.setRoleWriteAccess('Operations', true);
-                            // console.log('orderACL', orderACL)
-                            order.setACL(orderACL);
-                        }
-                        console.log('order.existed()', order.existed())
-                        console.log('order.getACL()', order.getACL())
-                    // } catch (e) {
-                    //     console.error(e)
-                    // }
-
-                    // check if request came from a User
-
-                    return calcOrderTotals(order);
-                    // return Promise.resolve(order);
-
-                })
                 .then(order => {
 
-                    if (!order.get('folio')) {
-                    console.log('another then')
+                    // Calculate Order's totals:
+                    return calcOrderTotals(order);
 
+                })
+
+                .then(order => {
+
+                    // Check if Order has already a folio:
+                    if (!order.get('folio')) {
+
+                        // Set Order's folio:
                         let query = new Parse.Query(Order);
                         query.limit(1);
                         query.descending('folio');
                         query.first()
                             .then(_order => {
-                                console.log('order', _order, _order.get('folio'), _order.folio)
                                 let maxValue = _order.get('folio');
                                 order.set('folio', ++maxValue);
-                                // order.save(null, {useMasterKey: true})
                                 order.save(null, {sessionToken: sessionToken})
                                     .then(() => {
                                         res.success();
                                     });
 
                             });
-                        // res.error('you cannot give less than one star');
 
                     } else {
                         order.save(null, {useMasterKey: true})
@@ -95,20 +57,50 @@ module.exports.load = () => {
 
 };
 
+function setOrderACL(order) {
+    return new Promise((resolve, reject) => {
+
+        let client = order.client;
+        let clientQuery = new Parse.Query(Client);
+        return clientQuery
+            .equalTo('objectId', client.id)
+            .include('promoter')
+            .include('user')
+            .first()
+            .then(client => {
+                if (!order.existed()) {
+                    let orderACL = new Parse.ACL();
+                    orderACL.setWriteAccess(client.user, true);
+                    orderACL.setReadAccess(client.user, true);
+                    orderACL.setWriteAccess(client.promoter.id, true);
+                    orderACL.setReadAccess(client.promoter.id, true);
+                    orderACL.setRoleReadAccess('Operations', true);
+                    orderACL.setRoleWriteAccess('Operations', true);
+                    order.setACL(orderACL);
+                }
+
+                resolve(order);
+            });
+
+    });
+
+}
 
 function calcOrderTotals(order) {
-    let deferred = q.defer();
 
-    let itemsRelation = order.relation('items');
-    itemsRelation.query().include('product').find()
-        .then(items => {
-            order.items = items;
-            order.set('subtotals', order.subtotals);
-            order.set('ivaTotals', order.ivaTotals);
-            order.set('discountTotals', order.discountTotals);
-            order.set('totals', order.totals);
-            deferred.resolve(order);
-        });
+    return new Promise((resolve, reject) => {
 
-    return deferred.promise;
+        let itemsRelation = order.relation('items');
+        itemsRelation.query().include('product').find()
+            .then(items => {
+                order.items = items;
+                order.set('subtotals', order.subtotals);
+                order.set('ivaTotals', order.ivaTotals);
+                order.set('discountTotals', order.discountTotals);
+                order.set('totals', order.totals);
+                resolve(order);
+            });
+
+    });
+
 }
